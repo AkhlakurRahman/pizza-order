6de997c873697fda418001c1a92502ea25b002b9
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-10-20 02:50:04 +0600

    sanity schema configuration added

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,62 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  name: 'pizza',
+  title: 'Pizza',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'pirce',
+      title: 'Pirce',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      validation: (Rule) => Rule.min(1000),
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ name, media, ...toppings }) => {
+      const tops = Object.values(toppings).filter(Boolean);
+      // which is equivalent to -> const tops = Object.values(toppings).filter((top) => top !== undefined);
+
+      return { title: name, media, subtitle: tops.join(', ') };
+    },
+  },
+};
---
+++ sanity/schemas/topping.js
@@ -0,0 +1,31 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  name: 'topping',
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is vegetarian?',
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🌱' : ''}`,
+    }),
+  },
+};

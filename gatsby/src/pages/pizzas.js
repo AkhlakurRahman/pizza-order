4abc62b3ab786521ac2d6d9b26a73489b1b9be46
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-11-07 01:59:30 +0600

    all toppings for pizza has filtered and showing

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 import { PizzaList } from '../components/PizzaList/PizzaList';
+import { ToppingsFilter } from '../components/PizzaList/ToppingsFilter';
 
 export default function PizzasPage({ data }) {
   const pizzas = data.pizzas.nodes;
 
   return (
     <>
+      <ToppingsFilter />
       <PizzaList pizzas={pizzas} />
     </>
   );
